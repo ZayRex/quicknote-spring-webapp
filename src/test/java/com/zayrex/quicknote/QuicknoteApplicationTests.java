3197df27e02146f68671bce9c35f1cd843e47a63
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-03-25 18:10:04 +0000

    bootstrap

---
+++ src/test/java/com/zayrex/quicknote/QuicknoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayrex.quicknote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicknoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
