385c4fc18c953f68ac757427b26459e8483cb1c9
ZayRex <31854984+ZayRex@users.noreply.github.com>
2023-03-26 17:34:58 +0100

    notesController

--- src/main/java/com/zayrex/quicknote/QuicknoteApplication.java
+++ src/main/java/com/zayrex/quicknote/QuicknoteApplication.java
@@ -1,13 +1,104 @@
 package com.zayrex.quicknote;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class QuicknoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuicknoteApplication.class, args);
+	}
+
+}
+
+@Document(collection = "notes")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+class Note {
+	@Id
+	private String id;
+	private String textContent;
+
+	@Override
+	public String toString() {
+		return textContent;
+	}
+}
+interface NotesRepository extends MongoRepository<Note, String> {
+
+}
+
+@Controller
+class QuicknoteController {
+
+	@Autowired
+	private NotesRepository notesRepository;
+
+	@GetMapping("/")
+	public String index(Model model) {
+		getAllNotes(model);
+		return "index";
+	}
+
+	private void getAllNotes(Model model) {
+		List<Note> notes = notesRepository.findAll();
+		Collections.reverse(notes);
+		model.addAttribute("notes", notes);
+	}
+	private void saveNote(String textContent, Model model) {
+		if (textContent != null && !textContent.trim().isEmpty()) {
+			notesRepository.save(new Note(null, textContent.trim()));
+			//clear text field
+			model.addAttribute("textContent", "");
+		}
+	}
+	@PostMapping("/note")
+	public String saveNotes(@RequestParam("image") MultipartFile file,
+							@RequestParam String textContent,
+							@RequestParam(required = false) String save,
+							@RequestParam(required = false) String upload,
+							Model model) throws IOException {
+
+		if (save != null && save.equals("Save")) {
+			saveNote(textContent, model);
+			getAllNotes(model);
+			return "redirect:/";
+		}
+		// After save fetch all notes again
+		return "index";
 	}
 
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -4,8 +4,8 @@
     <link rel="stylesheet" href="tachyons.min.css">
 </head>
 <body class="ph3 pt0 pb4 mw7 center sans-serif">
-<h1 class="f2 mb0"><span class="gold">k</span>note</h1>
-<p class="f5 mt1 mb4 lh-copy">A simple note-taking app.</p>
+<h1 class="f2 mb0"><span class="red">Quick</span>note</h1>
+<p class="f5 mt1 mb4 lh-copy">A quick way to write and store your notes online</p>
 <form action="/note" method="POST" enctype="multipart/form-data">
     <ol class="list pl0">
         <li class="mv3">
@@ -15,11 +15,11 @@
             <input class="f6 link dim br1 ba bw1 ph3 pv2 mb2 dib black bg-white pointer ml2" type="submit"
                    value="Upload" name="upload">
         </li>
-        <li class="mv3"><label class="f6 b db mb2" for="description">Write your content here</label>
+        <li class="mv3"><label class="f6 b db mb2" for="textContent">Write your content here</label>
             <textarea class="f4 db border-box hover-black w-100 measure ba b--black-20 pa2 br2 mb2" rows="5"
-                      name="description"><#if description??>${description}</#if></textarea>
-            <input class="f6 link dim br1 ba bw1 ph3 pv2 mb2 dib black bg-white pointer" type="submit" value="Publish"
-                   name="publish">
+                      name="textContent"><#if textContent??>${textContent}</#if></textarea>
+            <input class="f6 link dim br1 ba bw1 ph3 pv2 mb2 dib black bg-white pointer" type="submit" value="Save"
+                   name="Save">
         </li>
     </ol>
 </form>
